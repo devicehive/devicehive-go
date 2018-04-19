f2ff875a578c2287fe68221367271976066ac79e
Alexei Torunov <merisho@ukr.net>
2018-04-19 16:27:22 +0300

    add test for device update

--- integrationtest/ws/device_test.go
+++ integrationtest/ws/device_test.go
@@ -3,24 +3,38 @@ package dh_test
 import (
 	"testing"
 	"github.com/matryer/is"
+	"github.com/devicehive/devicehive-go/dh"
 )
 
 func TestDevice(t *testing.T) {
 	is := is.New(t)
 
-	err := client.PutDevice("go-test-dev", nil)
+	device := &dh.Device{
+		Name: "go test",
+	}
+
+	err := client.PutDevice("go-test-dev", device)
 	if err != nil {
 		t.Errorf("%s: %v", err.Name(), err)
 		return
 	}
 
-	device, err := client.GetDevice("go-test-dev")
+	device.Name = "go test updated"
+
+	err = client.PutDevice(device.Id, device)
+	if err != nil {
+		t.Errorf("%s: %v", err.Name(), err)
+		return
+	}
+
+	device, err = client.GetDevice(device.Id)
 	if err != nil {
 		t.Errorf("%s: %v", err.Name(), err)
 		return
 	}
 
 	is.True(device != nil)
+	is.Equal(device.Name, "go test updated")
 
 	err = client.RemoveDevice("go-test-dev")
 	if err != nil {
