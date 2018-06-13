9b5dcdab9c5ca2426eca28c2a63e320503f9af53
Alexei Torunov <merisho@ukr.net>
2018-06-13 09:24:23 +0300

    Refactor handleSubscriptionError

--- client.go
+++ client.go
@@ -80,9 +80,8 @@ func (c *Client) SubscribeCommands(params *SubscribeParams) (*CommandSubscriptio
 }
 
 func (c *Client) handleSubscriptionError(subs subscriber, err error) {
-	if err.Error() == TokenExpiredErr {
-		if subscriptionReauth.reauthNeeded() {
-			c.reauthenticateSubscription(subs)
+	if err.Error() == TokenExpiredErr && subscriptionReauth.reauthNeeded() {
+		if res := c.reauthenticateSubscription(subs); res {
 			subscriptionReauth.reauthPoint()
 		}
 	} else {
@@ -90,19 +89,22 @@ func (c *Client) handleSubscriptionError(subs subscriber, err error) {
 	}
 }
 
-func (c *Client) reauthenticateSubscription(subs subscriber) {
+func (c *Client) reauthenticateSubscription(subs subscriber) bool {
 	accessToken, err := c.RefreshToken()
 	if err != nil {
 		removeSubscriptionWithError(subs, err)
-		return
+		return false
 	}
 
 	if success, err := c.authenticate(accessToken); err != nil {
 		removeSubscriptionWithError(subs, err)
-		return
+		return false
 	} else if !success {
 		removeSubscriptionWithError(subs, newError(errors.New("re-authentication failed")))
+		return false
 	}
+
+	return true
 }
 
 func (c *Client) authenticate(token string) (bool, *Error) {
